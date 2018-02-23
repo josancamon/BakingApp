b97cbed4b197348cead5ea05160be82d28c8881d
Santiago <joan.santiago.cabezas@gmail.com>
2018-02-22 21:10:25 -0500

    without espresso

--- app/src/main/java/com/example/santiago/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/santiago/bakingapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.santiago.bakingapp;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.pm.ShortcutInfo;
 import android.content.pm.ShortcutManager;
@@ -18,7 +19,7 @@ import com.example.santiago.bakingapp.Model.Recipe;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity implements RecipesListFragment.OnRecipeClickListener,RecipesListFragment.AddShorcut {
+public class MainActivity extends AppCompatActivity implements RecipesListFragment.OnRecipeClickListener, RecipesListFragment.AddShorcut {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,17 +51,34 @@ public class MainActivity extends AppCompatActivity implements RecipesListFragme
     public void addShortcut(Recipe recipeAdded) {
         setset();
     }
-    public void setset(){
-        ShortcutManager shortcutManager = getSystemService(ShortcutManager.class);
 
-        ShortcutInfo shortcut = new ShortcutInfo.Builder(this, "id1")
-                .setShortLabel("Web site")
-                .setLongLabel("Open the web site")
-                .setIcon(Icon.createWithResource(this, R.drawable.add_logo))
-                .setIntent(new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://www.mysite.example.com/")))
-                .build();
+    public void setset() {
+        ShortcutManager mShortcutManager = getSystemService(ShortcutManager.class);
+        if (mShortcutManager.isRequestPinShortcutSupported()) {
+            // Assumes there's already a shortcut with the ID "my-shortcut".
+            // The shortcut must be enabled.
+            ShortcutInfo pinShortcutInfo =
+                    new ShortcutInfo.Builder(this, "my-shortcut").setShortLabel("asdas").
+                            setLongLabel("Open the web site")
+                            .setIcon(Icon.createWithResource(this, R.drawable.add_logo))
+                            .setIntent(new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("https://www.mysite.example.com/"))).build();
 
-        shortcutManager.setDynamicShortcuts(Arrays.asList(shortcut));
+            // Create the PendingIntent object only if your app needs to be notified
+            // that the user allowed the shortcut to be pinned. Note that, if the
+            // pinning operation fails, your app isn't notified. We assume here that the
+            // app has implemented a method called createShortcutResultIntent() that
+            // returns a broadcast intent.
+            Intent pinnedShortcutCallbackIntent =
+                    mShortcutManager.createShortcutResultIntent(pinShortcutInfo);
+
+            // Configure the intent so that your app's broadcast receiver gets
+            // the callback successfully.
+            PendingIntent successCallback = PendingIntent.getBroadcast(this, 0,
+                    pinnedShortcutCallbackIntent, 0);
+
+            mShortcutManager.requestPinShortcut(pinShortcutInfo,
+                    successCallback.getIntentSender());
+        }
     }
 }
